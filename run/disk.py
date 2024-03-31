d934196c5fd7c41201489dbf2fa823e1ea1d40dd
Frank Holler <holler.frank@gmail.com>
2024-03-31 18:36:22 +0200

    Mounten von Laufwerken

--- run/disk.py
+++ run/disk.py
@@ -32,6 +32,22 @@ async def format_drive(drive: str, mode: str)->None:
         print()
         print('\n'.join(err))
 
+async def mount(drive: str, folder: str)->None:
+    p = await asyncio.subprocess.create_subprocess_shell(f'sudo mkdir -p {folder}', 
+                                                         stderr=asyncio.subprocess.PIPE, 
+                                                         stdout=asyncio.subprocess.PIPE)
+    await p.wait()
+    p = await asyncio.subprocess.create_subprocess_shell(f'sudo mount /dev/{drive} {folder}', 
+                                                         stderr=asyncio.subprocess.PIPE, 
+                                                         stdout=asyncio.subprocess.PIPE)
+    await p.wait()
+
+async def umount(folder: str)->None:
+    p = await asyncio.subprocess.create_subprocess_shell(f'sudo umount {folder}', 
+                                                         stderr=asyncio.subprocess.PIPE, 
+                                                         stdout=asyncio.subprocess.PIPE)
+    await p.wait()
+
 async def get_drives():
     p = await asyncio.subprocess.create_subprocess_shell('lsblk -J -o UUID,NAME,SIZE,MOUNTPOINT,FSTYPE', 
                                                          stderr=asyncio.subprocess.PIPE, 
@@ -50,12 +66,23 @@ async def main()->None:
     format.add_argument("drive", type=str, help='Laufwerk ohne /dev/')
     format.add_argument("-t", '--type', choices=['ext4', 'vfat'], default='ext4', help='Laufwerk ohne /dev/')
     format.set_defaults(mode='format')
+    format = subparser.add_parser("mount", help='Mounten eines Laufwerkes')
+    format.add_argument("drive", type=str, help='Laufwerk ohne /dev/')
+    format.add_argument("folder", type=str, help='Order an den gebunden werden soll')
+    format.set_defaults(mode='mount')
+    format = subparser.add_parser("umount", help='Unmounten eines Laufwerkes')
+    format.add_argument("folder", type=str, help='Order an dem entfernt werden soll')
+    format.set_defaults(mode='umount')
     #parser.add_argument('-f', action='store_true', dest='format', help='Formatieren von Laufwerk')
     #subparsers.add_argument('-d', '--drive', dest='drive', help='Name des Laufwerkes')
     #subparsers.add_argument('--formattype', dest='format_type', help='Name des Laufwerkes', choices=['ext4', 'vfat'], default='ext4')
     args = parser.parse_args()
     if hasattr(args, 'mode') and args.mode == 'format':
         await format_drive(args.drive, args.type)
+    elif hasattr(args, 'mode') and args.mode == 'mount':
+        await mount(args.drive, args.folder)
+    elif hasattr(args, 'mode') and args.mode == 'umount':
+        await umount(args.folder)
     else:
         async for entry in get_drives():
             print(f'{entry[0]: <15}{entry[1]: >6} {entry[2]: <25} {entry[4]: <6} {entry[3]} ')
